b3008964414ca13f7eecda08007bcc1131fb5ad2
cleiton.cabral <cleitoncabral177@hotmail.com>
2023-07-10 22:20:42 -0300

    setup front-end

---
+++ src/pages/AuthForm/AuthForm.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+
+type Variant = 'LOGIN' | 'REGISTER'
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>('LOGIN')
+  const [isLoading, setIsLoading] = useState()
+}
